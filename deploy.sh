2bec75590b7d90c4b6854bf706ba87cd56c9eb2d
Peter Phillips <github@pmphillips.ca>
2019-04-22 17:46:51 -0700

    Uncommented lines

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-#pkill -f aragon
-#aragon ipfs &
-#aragon devchain --reset &
-#sleep 10
-#truffle migrate --reset
+pkill -f aragon
+aragon ipfs &
+aragon devchain --reset &
+sleep 10
+truffle migrate --reset
 TOKEN=$( jq -r .Token contracts.json )
 TOKENSALE=$( jq -r .Tokensale contracts.json )
-#cd apps/MyTokens
-#aragon apm publish major --files app/build
-#cd ../MyID
-#aragon apm publish major --files app/build
-#cd ../MyBitTemplate
-cd apps/MyBitTemplate
+cd apps/MyTokens
+aragon apm publish major --files app/build
+cd ../MyID
+aragon apm publish major --files app/build
+cd ../MyBitTemplate
 aragon run --template MyBitTemplate --template-init @ARAGON_ENS $TOKEN $TOKENSALE
